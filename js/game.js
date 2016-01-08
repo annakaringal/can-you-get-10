c6bd6bc870b22908288b3a809d86de57722c2180
Anna Karingal <anna.karingal@gmail.com>
2016-01-08 13:32:35 -0500

    check each cell to see if it can be merged

--- js/game.js
+++ js/game.js
@@ -4,6 +4,13 @@ function Game(options){
   this.highestNumber = options.highestNumber || 4;
   this.goalNumber = 10;
 
+  this.directions = {
+    0: {x: 0, y:  -1}, // up
+    1: {x: 1, y: 0}, // right
+    2: {x: 0, y: 1}, // down
+    3: {x: -1, y: 0}  // left
+  };
+
   this.setup();
 }
 
@@ -16,5 +23,32 @@ Game.prototype.won = function() {
 };
 
 Game.prototype.finished = function(){
-  this.won() || !this.mergeAvailable();
+  this.won() || !this.mergesAvailable();
 };
+
+// Check each tile in grid to see if it can be merged
+// TODO: make more efficient by skipping tiles who can be merged with neighbor
+// that has already been checked
+Game.prototype.mergesAvailable = function(){
+  for (var row=0; row<size; row++){
+    for (var col=0; col<size; col++){
+      if (canMerge(row, col) return true;
+    }
+  }
+  return false;
+};
+
+Game.prototype.directionVector = function(dir){
+  return this.directions[dir];
+};
+
+Game.prototype.canMerge = function(row, col){
+  for (var dir=0; dir<4; dir++){
+    var vec = directionVector(dir);
+    var cell = this.grid.cell(row, col);
+    var adjacent = this.grid.cell(row + vec.x, col + vec.y);
+
+    if (cell === adjacent) return true;
+  }
+  return false;
+}
