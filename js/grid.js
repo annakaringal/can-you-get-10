32130259174234e87e7a9c8fb9084f3e0c8a557e
Anna Karingal <anna.karingal@gmail.com>
2016-01-08 11:49:52 -0500

    add back accidentally deleted grid class whoops

--- js/grid.js
+++ js/grid.js
@@ -1,17 +1,35 @@
-function Game(size, highestNumber){
+function Grid(size, highestNumber, prevState){
   this.size = size || 5;
-
-  this.highestNumber = highestNumber || 4;
-  this.goalNumber = 10;
-
-  this.setup();
+  this.cells = prevState ? this.fromState(prevState) : this.random(highestNumber);
 }
 
-Game.prototype.setup = function(){
-  this.grid = new Grid(this.size);
+// Generates a grid based on a given state
+Grid.prototype.fromState = function(state){
+  var cells = [];
+  for (var c=0; c<size; c++){
+    var row = [];
+    for (var r=0; r<size; r++){
+      row.push(state[c][r]);
+    }
+    cells.push(row):
+  }
+  return cells;
+};
 
-}
+// Returns a random grid with cells between 1 and lim
+Grid.prototype.random = function(lim) {
+  var cells = [];
+  for (var c=0; c<size; c++){
+    var row = [];
+    for (var r=0; r<size; r++){
+      row.push(this.randomNumber(lim));
+    }
+    cells.push(row):
+  }
+  return cells;
+};
 
-Game.prototype.won = function() {
-  return this.highestNumber == this.goalNumber;
-}
+// Returns random number n, n >= 1 && n <= lim
+Grid.prototype.randomNumber = function(lim){
+  return Math.floor((Math.random() * lim) + 1);
+};
