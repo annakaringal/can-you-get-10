5cff62e998c882d07d2f4a34d46f43af5a46b7d2
Anna Karingal <anna.karingal@gmail.com>
2016-01-08 15:39:21 -0500

    test constructor initializes non empty correct sized 2D array

--- spec/GridSpec.js
+++ spec/GridSpec.js
@@ -5,12 +5,14 @@ describe("Grid", function() {
     grid = new Grid();
   });
 
-  it("should create a new non empty grid with random numbers, given no state", function() {
-
+  it("should create a grid of a default size 5x5", function() {
+    var randomRow = Math.floor(Math.random()*5);
+    expect(grid.cells.length).toEqual(5);
+    expect(grid.cells[randomRow].length).toEqual(5);
   });
 
-  it("should create a new non empty grid containing the state, given a state", function() {
-
+  it("should create a new non empty grid with random numbers, given no state", function() {
+    expect(grid).toBeNonEmpty(grid);
   });
 
 });
