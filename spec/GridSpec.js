5bd000857872a76c53b07f469d79120f9d8e2e99
Anna Karingal <anna.karingal@gmail.com>
2016-01-08 12:42:16 -0500

    skeleton game & grid spec

---
+++ spec/GridSpec.js
@@ -0,0 +1,16 @@
+describe("Grid", function() {
+  var grid;
+
+  beforeEach(function() {
+    grid = new Grid();
+  });
+
+  it("should create a new non empty grid with random numbers, given no state", function() {
+
+  });
+
+  it("should create a new non empty grid containing the state, given a state", function() {
+
+  });
+
+});
