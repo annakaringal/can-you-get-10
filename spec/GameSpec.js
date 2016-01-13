3392d8acfaa2c22000ec0e5618590c60872d7a48
Anna Karingal <anna.karingal@gmail.com>
2016-01-13 10:32:40 -0500

    test check for mergeable cell

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -20,4 +20,25 @@ describe("Game", function() {
     expect(game.adjacentCell(randomRow, randomCol, up).content).toEqual(cellAbove);
   });
 
+  it ('returns whether or not the cell is mergeable', function(){
+    var randomRow = Math.floor(Math.random()*(gridSize-1));
+    var randomCol = Math.floor(Math.random()*(gridSize-1));
+    var cell = game.grid.cell(randomRow, randomCol);
+
+    var mergeable = false;
+    for (var dir=0; dir < 4; dir++){
+      var adjacent = game.adjacentCell(randomRow, randomCol, dir);
+      if (!adjacent) continue;
+      if (adjacent.content == cell){
+        mergeable = true;
+        break;
+      }
+    }
+    expect(function(){
+      var can = game.canMerge(randomRow, randomCol);
+      console.log(can);
+      return can;
+    }()).toEqual(mergeable);
+  });
+
 });
