ee359f139ceed6d970da88d33e904d99dc79ab8c
Anna Karingal <anna.karingal@gmail.com>
2016-01-22 11:22:40 -0500

    group tests by functionality

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -10,48 +10,54 @@ describe("Game", function() {
     randomCol = Math.floor(Math.random()*(gridSize-1));
   });
 
-  it ('should create a non-empty grid of random numbers', function(){
-    expect(game.grid).toBeNonEmpty(game.grid);
+  describe('construct a new game', function(){
+    it ('should create a non-empty grid of random numbers', function(){
+      expect(game.grid).toBeNonEmpty(game.grid);
+    });
   });
 
-  it ('returns the correct adjacent value', function(){
-    if (randomRow === 0){
-      randomRow = 1;
-    }
-    var up = 0;
-    var cellAbove = game.grid.cell(randomRow-1, randomCol);
-    expect(game.adjacentCell(randomRow, randomCol, up).c).toEqual(randomCol);
-    expect(game.adjacentCell(randomRow, randomCol, up).r).toEqual(randomRow-1);
-    expect(game.adjacentCell(randomRow, randomCol, up).value).toEqual(cellAbove);
-  });
-
-  it ('returns whether or not the cell is mergeable', function(){
-    var cell = game.grid.cell(randomRow, randomCol);
-    var mergeable = false;
-    for (var dir=0; dir < 4; dir++){
-      var adjacent = game.adjacentCell(randomRow, randomCol, dir);
-      if (!adjacent) continue;
-      if (adjacent.value == cell){
-        mergeable = true;
-        break;
+  describe('check adjacent cells', function(){
+    it ('returns the correct adjacent value', function(){
+      if (randomRow === 0){
+        randomRow = 1;
       }
-    }
-    expect(game.canMerge(randomRow, randomCol)).toEqual(mergeable);
+      var up = 0;
+      var cellAbove = game.grid.cell(randomRow-1, randomCol);
+      expect(game.adjacentCell(randomRow, randomCol, up).c).toEqual(randomCol);
+      expect(game.adjacentCell(randomRow, randomCol, up).r).toEqual(randomRow-1);
+      expect(game.adjacentCell(randomRow, randomCol, up).value).toEqual(cellAbove);
+    });
   });
 
-  it ("increments correct cell when merged", function(){
-    game.merge(0,2);
-    expect(game.grid.cell(2,2)).toEqual(3);
-  });
+  describe ('merging cells', function(){
+    it ('returns whether or not the cell is mergeable', function(){
+      var cell = game.grid.cell(randomRow, randomCol);
+      var mergeable = false;
+      for (var dir=0; dir < 4; dir++){
+        var adjacent = game.adjacentCell(randomRow, randomCol, dir);
+        if (!adjacent) continue;
+        if (adjacent.value == cell){
+          mergeable = true;
+          break;
+        }
+      }
+      expect(game.canMerge(randomRow, randomCol)).toEqual(mergeable);
+    });
 
-  it ('increments highest number in the game when current highest number is merged', function(){
-    game.merge(1,1);
-    expect(game.highestNumber).toEqual(5);
-  });
+    it ("increments correct cell when merged", function(){
+      game.merge(0,2);
+      expect(game.grid.cell(2,2)).toEqual(3);
+    });
 
-  it ('does nothing when a cell cannot be merged', function(){
-    game.merge(0,0);
-    expect(game.grid.toString()).toEqual(state);
+    it ('increments highest number in the game when current highest number is merged', function(){
+      game.merge(1,1);
+      expect(game.highestNumber).toEqual(5);
+    });
+
+    it ('does nothing when a cell cannot be merged', function(){
+      game.merge(0,0);
+      expect(game.grid.toString()).toEqual(state);
+    });
   });
 
 
--- spec/GridSpec.js
+++ spec/GridSpec.js
@@ -10,60 +10,66 @@ describe("Grid", function() {
     randomCol = Math.floor(Math.random()*(gridSize-1));
   });
 
-  it("should create a grid of a default size 5x5", function() {
-    expect(grid.cells.length).toEqual(gridSize);
-    expect(grid.cells[randomRow].length).toEqual(gridSize);
-  });
-
-  it("should create a new non empty grid with random numbers, given no state", function() {
-    expect(grid).toBeNonEmpty(grid);
-  });
-
-  it("should create a grid from a given state", function() {
-    var gridFromState = new Grid({state: state});
-    expect(gridFromState.cells).toEqual([ [2,1,2,1,1],
-                                          [3,4,2,2,2],
-                                          [3,3,2,3,2],
-                                          [2,3,3,1,1],
-                                          [3,1,1,3,1] ]);
-  });
-
-  it("should construct a new non empty grid with numbers between 1 and highestNumber", function() {
-    var highestNumber = Math.floor(Math.random()*10 + 1);
-    var newGrid = new Grid({highestNumber: highestNumber});
-
-    var actualHighest = 0;
-    var lessThanOne = false;
-    newGrid.eachCell(function(row, col){
-      var cell = newGrid.cell(row, col);
-      if (cell > actualHighest){
-        actualHighest = cell;
-      }
-      if (cell < 1){
-        lessThanOne = true;
-      }
+  describe ('construct a new grid', function(){
+    it("should create a grid of a default size 5x5", function() {
+      expect(grid.cells.length).toEqual(gridSize);
+      expect(grid.cells[randomRow].length).toEqual(gridSize);
     });
 
-    expect(lessThanOne).toBeFalsy;
-    expect(actualHighest).not.toBeGreaterThan(highestNumber);
+    it("should create a new non empty grid with random numbers, given no state", function() {
+      expect(grid).toBeNonEmpty(grid);
+    });
+
+    it("should create a grid from a given state", function() {
+      var gridFromState = new Grid({state: state});
+      expect(gridFromState.cells).toEqual([ [2,1,2,1,1],
+                                            [3,4,2,2,2],
+                                            [3,3,2,3,2],
+                                            [2,3,3,1,1],
+                                            [3,1,1,3,1] ]);
+    });
+
+    it("should construct a new non empty grid with numbers between 1 and highestNumber", function() {
+      var highestNumber = Math.floor(Math.random()*10 + 1);
+      var newGrid = new Grid({highestNumber: highestNumber});
+
+      var actualHighest = 0;
+      var lessThanOne = false;
+      newGrid.eachCell(function(row, col){
+        var cell = newGrid.cell(row, col);
+        if (cell > actualHighest){
+          actualHighest = cell;
+        }
+        if (cell < 1){
+          lessThanOne = true;
+        }
+      });
+
+      expect(lessThanOne).toBeFalsy;
+      expect(actualHighest).not.toBeGreaterThan(highestNumber);
+    });
   });
 
-  it ("should set and return the correct cell number", function(){
-    var val = 1;
-    grid.setCell(randomRow, randomCol, val);
-    expect(grid.cell(randomRow, randomCol)).toEqual(val);
+  describe ('set cell value', function(){
+    it ("should set and return the correct cell number", function(){
+      var val = 1;
+      grid.setCell(randomRow, randomCol, val);
+      expect(grid.cell(randomRow, randomCol)).toEqual(val);
+    });
+
+    it ("should not set cell to an invalid value", function(){
+      var oldVal = grid.cell(randomRow, randomCol);
+      var newVal = -1;
+      grid.setCell(randomRow, randomCol, newVal);
+      expect(grid.cell(randomRow, randomCol)).toEqual(oldVal);
+    });
   });
 
-  it ("should not set cell to an invalid value", function(){
-    var oldVal = grid.cell(randomRow, randomCol);
-    var newVal = -1;
-    grid.setCell(randomRow, randomCol, newVal);
-    expect(grid.cell(randomRow, randomCol)).toEqual(oldVal);
-  });
-
-  it("should return the grid's current state as a string", function() {
-    var gridFromState = new Grid({state: state});
-    expect(gridFromState.toString()).toEqual(state);
+  describe ('retrieve grid state', function(){
+    it("should return the grid's current state as a string", function() {
+      var gridFromState = new Grid({state: state});
+      expect(gridFromState.toString()).toEqual(state);
+    });
   });
 
 });
